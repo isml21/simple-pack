f667d40a777352429e2ea923a911febace4cbfa9
Vick03-rnf <vickyrnf78@gmail.com>
2025-01-05 13:11:43 +0700

    hhhh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="footer">
       <div className="container">
         <p>© 2024 PackMael. All rights reserved.</p>
-        <p>Hubungi kami: <a href="mailto:hello@packmael.com">hello@packmael.com</a> | 0896-4959-7039</p>
+        <p>Hubungi kami: <a href="mailto:hello@packmael.com">@packmael.com</a> | 0812-3456-7890</p>
       </div>
     </footer>
   );
